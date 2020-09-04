2e5ec0351cad3f0e1686584bcccc919839e59b07
Joy Evans <joyevans@me.com>
2020-09-04 21:02:15 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,6 +1,6 @@
 current_time = Time.now 
-current_time = current_time.to __LINE__
-if current_time % 2 == log10
+current_time = current_time.to_i 
+if current_time % 2 == 0 
   puts "Even!"
 else
   puts "Odd!"
